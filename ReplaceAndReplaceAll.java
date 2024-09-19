180314022f0ae83cf8b5885435f6e8c8ece0ebc6
ganeshkandhan17 <ganeshkandhan@gmail.com>
2024-09-19 12:09:47 +0530

    Update

---
+++ ReplaceAndReplaceAll.java
@@ -0,0 +1,8 @@
+public class ReplaceAndReplaceAll{
+    public static void main(String args[]){
+        String str1="This is is is of of this of";
+        String str2=str1.replaceAll("is","a");
+        System.out.print(str2);
+
+    }
+}
